955514f4df06bd87ac081d3d4ce44ac508fc312f
hochbruj <jhochbrueckner@gmail.com>
2020-06-15 10:32:59 +1000

    Updates

---
+++ precache-manifest.5aefa6b0d6968388f2488e523907df74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803bf5722c826014957afa6c7499264f",
+    "url": "/defi-portal/index.html"
+  },
+  {
+    "revision": "1d8b99bcdbda72bd2e69",
+    "url": "/defi-portal/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "65292289d585629d5fbc",
+    "url": "/defi-portal/static/css/main.fec45a73.chunk.css"
+  },
+  {
+    "revision": "1d8b99bcdbda72bd2e69",
+    "url": "/defi-portal/static/js/2.0730c707.chunk.js"
+  },
+  {
+    "revision": "ecf17d265291e8c39c830a92cbfd263e",
+    "url": "/defi-portal/static/js/2.0730c707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65292289d585629d5fbc",
+    "url": "/defi-portal/static/js/main.513333c4.chunk.js"
+  },
+  {
+    "revision": "5ca223267cd7d49b7a2b",
+    "url": "/defi-portal/static/js/runtime-main.c889c47c.js"
+  }
+]);
